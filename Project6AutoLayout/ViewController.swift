e31e6d31c9e3567360f9a657547fb2121a2f9d2e
henry22 <henry811010@gmail.com>
2015-05-17 14:11:42 +0800

    We're going to do is add some constraints that say each label should start at the left edge of its superview, and end at the right edge, by using a technique called Auto Layout Visual Format Language (VFL).

--- Project6AutoLayout/ViewController.swift
+++ Project6AutoLayout/ViewController.swift
@@ -12,7 +12,43 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let label1 = UILabel()
+        label1.setTranslatesAutoresizingMaskIntoConstraints(false)
+        label1.backgroundColor = UIColor.redColor()
+        label1.text = "THESE"
+        
+        let label2 = UILabel()
+        label2.setTranslatesAutoresizingMaskIntoConstraints(false)
+        label2.backgroundColor = UIColor.cyanColor()
+        label2.text = "ARE"
+        
+        let label3 = UILabel()
+        label3.setTranslatesAutoresizingMaskIntoConstraints(false)
+        label3.backgroundColor = UIColor.yellowColor()
+        label3.text = "SOME"
+        
+        let label4 = UILabel()
+        label4.setTranslatesAutoresizingMaskIntoConstraints(false)
+        label4.backgroundColor = UIColor.greenColor()
+        label4.text = "AWESOME"
+        
+        let label5 = UILabel()
+        label5.setTranslatesAutoresizingMaskIntoConstraints(false)
+        label5.backgroundColor = UIColor.orangeColor()
+        label5.text = "LABELS"
+        
+        //All five views added to the view belonging to view controller
+        view.addSubview(label1)
+        view.addSubview(label2)
+        view.addSubview(label3)
+        view.addSubview(label4)
+        view.addSubview(label5)
+    }
+    
+    //Don't want to show the iOS status bar on this view controller
+    override func prefersStatusBarHidden() -> Bool {
+        return true
     }
 
     override func didReceiveMemoryWarning() {
