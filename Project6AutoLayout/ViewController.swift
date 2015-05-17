dbe1bcc44f2a946981b6f893c3d7c27bef60d8c6
henry22 <henry811010@gmail.com>
2015-05-17 14:55:57 +0800

    See all five labels stretching edge-to-edge horizontally, then spaced neatly vertically.

--- Project6AutoLayout/ViewController.swift
+++ Project6AutoLayout/ViewController.swift
@@ -50,6 +50,8 @@ class ViewController: UIViewController {
         for label in viewsDictionary.keys {
             view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|[\(label)]|", options: .allZeros, metrics: nil, views: viewsDictionary))
         }
+        //vertical VFL doesn't have a pipe at the end, so we're not forcing the last label to stretch all the way to the edge of our view
+        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|[label1]-[label2]-[label3]-[label4]-[label5]", options: .allZeros, metrics: nil, views: viewsDictionary))
     }
     
     //Don't want to show the iOS status bar on this view controller
